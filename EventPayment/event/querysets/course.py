a39a328ffc601334cfa6b3a478678ad198a96272
Bjørn Are Therkelsen <ba.therk@outlook.com>
2020-05-03 17:18:05 +0200

    Fix queryset for course

--- EventPayment/event/querysets/course.py
+++ EventPayment/event/querysets/course.py
@@ -8,7 +8,7 @@ class CourseQuerySet(models.QuerySet):
     def past(self):
         earlier_years = self.filter(year__lt=date.today().year)
         this_year = self.filter(year=date.today().year).filter(week_end__lt=get_current_week())
-        return earlier_years.union(this_year)
+        return earlier_years | this_year
 
     def present(self):
         return self.filter(year=date.today().year).filter(week_start__lte=get_current_week()).filter(week_end__gte=get_current_week())
@@ -16,4 +16,4 @@ class CourseQuerySet(models.QuerySet):
     def future(self):
         future_years = self.filter(year__gt=date.today().year)
         this_year = self.filter(year=date.today().year).filter(week_start__gt=get_current_week())
-        return future_years.union(this_year)
+        return future_years | this_year
