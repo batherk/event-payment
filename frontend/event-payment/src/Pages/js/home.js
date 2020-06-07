8b07ae604765f40abff8b3aeb9e71aecbb9ad2c5
Bjørn Are Therkelsen <ba.therk@outlook.com>
2020-06-08 01:41:19 +0200

    Update home page

--- frontend/event-payment/src/Pages/js/home.js
+++ frontend/event-payment/src/Pages/js/home.js
@@ -5,8 +5,8 @@ import { Parallax } from './../../Components/js'
 export default ()=>{
 
         const title = 'Salsa events and courses'
-        const imageURL= 'Profil.jpg'
-        const text= "This is a website for finding possible dance events and courses."
+        const imageURL= 'Couple.jpg'
+        const text= "This is a website for finding dance classes and events you want to join!<br>It is here you register and buy passes as well."
         const imageSide = "left"
 
   return (
